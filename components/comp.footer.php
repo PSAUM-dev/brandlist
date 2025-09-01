a0a094956921b06424390a8baa1e5eca5d3cbb39
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-09-01 20:51:37 +0100

    Update homepage content, improve footer styling

--- components/comp.footer.php
+++ components/comp.footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container-fluid text-white text-center">
-        <p>Brand TopList @2025</p>
+        <p class="bg-dark py-3"><b>Brand TopList @2025</b></p>
     </div>
 </footer>
--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,19 @@
 <main class="mb-2 section">
+
     <div class="container">
+
+        <h2 class="text-white">Top Brand List 2025</h2>
+        <p  class="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam fugiat quidem repellendus dolores quas
+            facilis rem molestiae sequi doloremque! Mollitia rerum porro reiciendis magni natus itaque aut. Facere, modi
+            eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam fugiat quidem repellendus dolores quas
+            facilis rem molestiae sequi doloremque! Mollitia rerum porro reiciendis magni natus itaque aut. Facere, modi
+            eveniet.
+        </p>
+
         <div class="row" id="topbrandlist">
             <p class="text-white">Loading data...</p>
-
-            
-
         </div>
     </div>
+
 </main>
