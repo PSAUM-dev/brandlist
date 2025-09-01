74f47f722274298d5177884dcf4c7f96b277627f
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-09-01 19:26:16 +0100

    Get brands data from API
    I've got brands data from API and printed 'em on the page

--- controller/controller.home.js
+++ controller/controller.home.js
@@ -1,1 +1,51 @@
-alert("Accueil");
+
+const BrandListUrlRequest = 'http://localhost/gestion/brands/';
+
+fetch (BrandListUrlRequest)
+
+    . then ((response) => response.json())
+    . then ((data) => {
+        
+        /* * Add brands on screen * */
+
+        // Div that containing brands data
+        const HtmlBransListContainer = document.getElementById("topbrandlist");
+        HtmlBransListContainer.innerHTML = '';
+
+        data.forEach((brand) => {
+            
+            const HtmlBrandElement = `
+
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <a href="#" class="px-4 py-4 bg-white text-center d-block match-height">
+                    <h3 class="mb-3 mt-0">${brand.brand_name}</h3>
+
+                </a>
+
+                <div class="px-4 bg-white text-center d-block">
+                    <p class="mb-0 display-5">${brand.rating}.0</p>
+                    <div>
+                        <div class="d-flex justify-content-center">
+                            <p><img src="assets/images/filledStar.svg" alt=""></p>
+                            <p><img src="assets/images/filledStar.svg" alt=""></p>
+                            <p><img src="assets/images/filledStar.svg" alt=""></p>
+                            <p><img src="assets/images/filledStar.svg" alt=""></p>
+                            <p><img src="assets/images/HalfFilledStar.svg" alt=""></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-center p-2 bg-light">
+                    <a href="">Plus d'informations</a>
+                </div>
+            </div>
+            
+            `;
+
+            HtmlBransListContainer.innerHTML += HtmlBrandElement;
+
+        });
+        
+    })
+
+    . catch ((error) => console.error("Error while fecthing brand data", error));
--- pages/home.php
+++ pages/home.php
@@ -1,24 +1,9 @@
 <main class="mb-2 section">
     <div class="container">
-        <div class="row">
+        <div class="row" id="topbrandlist">
+            <p class="text-white">Loading data...</p>
 
-            <div class="col-lg-4 col-sm-6 mb-4 aos-item" data-aos="zoom-out">
-                <a href="#" class="px-4 py-4 bg-white text-center d-block match-height">
-                    <h3 class="mb-3 mt-0">Nom du brand</h3>
-
-                </a>
-
-                <div class="px-4 bg-white text-center d-block">
-                    <p class="mb-0">5.0</p>
-                    <div>
-                        Les étoiles ici
-                    </div>
-                </div>
-
-                <div class="text-center p-2 bg-light">
-                    <a href="">Plus d'informations</a>
-                </div>
-            </div>
+            
 
         </div>
     </div>
