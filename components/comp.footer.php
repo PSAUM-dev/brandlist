cbcd1bbebc56cc2b57ef8bd0ff5ca3f292e439b8
PSAUM <83640023+PSAUM-dev@users.noreply.github.com>
2025-09-01 17:27:30 +0100

    First Commit | Project Structure

---
+++ components/comp.footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container-fluid text-white text-center">
+        <p>Brand TopList @2025</p>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $version = 1; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brand TopList</title>
+
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+
+    <!-- My custom ccss -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+</head>
+<body class="position-relative">
+
+    <?php
+
+        // Header
+        include 'components/comp.header.php';
+
+        // Page body
+        $pageIndex = isset($_GET['p']) ? $_GET['p'] : 'home';
+
+        switch ($pageIndex) {
+            case 'home' :
+                include "pages/$pageIndex.php";
+                break;
+
+            default :
+                include "pages/404.php";
+                break;
+        }
+
+        // Footer
+        include 'components/comp.footer.php';
+
+        // Page controller
+        echo "<script type='module' src='controller/controller.$pageIndex.js?version=$version'></script>";
+    ?>
+
+</body>
+</html>
